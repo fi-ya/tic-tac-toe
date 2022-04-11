9de0d4816fca4677fc9e182019700df125de6a90
fi-ya <safiaali@hotmail.com>
2022-04-11 10:39:25 +0100

    test: App renders heading on homepage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import Game from './components/Game'
 
-test('renders learn react link', () => {
+test('renders heading on the homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByRole('heading', {  name: /let's play tic tac toe!/i})
+  
+  expect(heading).toBeInTheDocument();
+});
+
+xtest('renders game component on the homepage', () => {
+  render(<App />);
+  const gameComponent = render(<Game/>)
+  
+  expect(gameComponent).toBeInTheDocument();
 });
