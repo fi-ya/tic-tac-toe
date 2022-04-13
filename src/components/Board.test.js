1f2a364c804fe5ab204b733bc84fb6e5a779fa59
fi-ya <safiaali@hotmail.com>
2022-04-13 17:34:29 +0100

    test: when play O marks the board

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -1,4 +1,4 @@
-import { render , screen, fireEvent} from "@testing-library/react";
+import { render , screen} from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
 import React from 'react';
 import Board from './Board';
@@ -7,41 +7,48 @@ import Board from './Board';
 describe('Board', ()=>{
   
   it('should render nine buttons', ()=>{
-    render(<Board
-      gridData={[1,2,3,4,5,6,7,8,9]} 
-      currentPlayerMarker='X'
-      
-   />);
+    render(<Board gridData={[1,2,3,4,5,6,7,8,9]} />);
     const gridButtonElements = screen.getAllByRole('button');
+
     expect(gridButtonElements.length).toBe(9);
   })
 
   it('should render a number,one in each square', async()=>{
-    render(<Board
-      gridData={[1,2,3,4,5,6,7,8,9]} 
-      currentPlayerMarker='X'
-      
-   />);
+    render(<Board gridData={[1,2,3,4,5,6,7,8,9]} />);
     const gridButtonElements = screen.getAllByRole('button');
+
     expect(gridButtonElements[0].textContent).toBe('1')
     expect(gridButtonElements[1].textContent).toBe('2')
     expect(gridButtonElements[2].textContent).toBe('3')
   })
 
-  it('player clicks on grid button ', async()=>{
+  it('player X clicks on grid button 1', ()=>{
     expect.assertions(2);
 
     const mockAddPlayerMarkerStub = jest.fn();
-    render(<Board
-      gridData={[1,2,3,4,5,6,7,8,9]} 
-      currentPlayerMarker='X'
-      addPlayerMarker={mockAddPlayerMarkerStub}
-   />);
+    const gridDataStub = [1,2,3,4,5,6,7,8,9];
+    const currentPlayerMarkerStub = 'X';
+    render(<Board gridData={gridDataStub} currentPlayerMarker={currentPlayerMarkerStub} addPlayerMarker={mockAddPlayerMarkerStub}/>);
 
     const gridButtonElementOne = screen.getByRole('button', {  name: /1/i});
     userEvent.click(gridButtonElementOne);
 
     expect(mockAddPlayerMarkerStub).toBeCalled();
-    expect(mockAddPlayerMarkerStub).toBeCalledWith([1,2,3,4,5,6,7,8,9], 'X', gridButtonElementOne.textContent);
+    expect(mockAddPlayerMarkerStub).toBeCalledWith(gridDataStub, currentPlayerMarkerStub, gridButtonElementOne.textContent);
+  })
+
+  it('player O clicks on grid button 2 ', async()=>{
+    expect.assertions(2);
+    
+    const mockAddPlayerMarkerStub = jest.fn();
+    const gridDataStub = ['X',2,3,4,5,6,7,8,9];
+    const currentPlayerMarkerStub = 'O';
+    render(<Board gridData={gridDataStub} currentPlayerMarker={currentPlayerMarkerStub} addPlayerMarker={mockAddPlayerMarkerStub}/>);
+
+    const gridButtonElementTwo = screen.getByRole('button', {  name: /2/i});
+    userEvent.click(gridButtonElementTwo);
+
+    expect(mockAddPlayerMarkerStub).toBeCalled();
+    expect(mockAddPlayerMarkerStub).toBeCalledWith(gridDataStub, currentPlayerMarkerStub, gridButtonElementTwo.textContent);
   })
 })
