155a03adcd9e5535ec0e239cf034186c07388b99
fi-ya <safiaali@hotmail.com>
2022-04-19 18:51:24 +0100

    fix: code coverage using babel-plugin-istanbul & @cypress/instrument-cra

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -12,10 +12,12 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-import '@cypress/code-coverage/support'
+
+
 
 // Import commands.js using ES2015 syntax:
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+import '@cypress/code-coverage/support'
